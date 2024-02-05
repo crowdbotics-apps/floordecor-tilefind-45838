1cc03b58640d8ff3d64a78690673fb7cfab4cdf4
Crowdbotics <team@crowdbotics.com>
2024-02-05 03:42:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floordecor-tilefind-45838",
+  url: "https://floordecor-tilefind-45838.botics.co",
+  api: "https://floordecor-tilefind-45838.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
